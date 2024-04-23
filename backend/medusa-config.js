d70f780aaa72b9ea8e76dae31e84ecc65d286c9f
Martin Gerard <mgerard@zenbranding.ca>
2024-04-23 14:41:39 -0400

    prepare pour digital Ocean

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -28,10 +28,22 @@ const ADMIN_CORS =
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 
-const DATABASE_URL =
-  process.env.DATABASE_URL || "postgres://localhost/medusa-starter-default";
+// const DATABASE_URL =
+//   process.env.DATABASE_URL || "postgres://localhost/medusa-starter-default";
 
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
+
+// This needs to be changed as DigitalOcean provides the different details of the database connection separately.
+const DB_USERNAME = process.env.DB_USERNAME
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_HOST = process.env.DB_HOST
+const DB_PORT = process.env.DB_PORT
+const DB_DATABASE = process.env.DB_DATABASE
+
+const DATABASE_URL = 
+  `postgres://${DB_USERNAME}:${DB_PASSWORD}` + 
+  `@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`
+
 
 const plugins = [
   `medusa-fulfillment-manual`,
@@ -87,11 +99,6 @@ module.exports = {
   modules,
   projectConfig: {
     // ...
-    database_extra: process.env.NODE_ENV !== "development" ?
-      {
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      } : {},
+    database_extra: { ssl: { rejectUnauthorized: false}},
   },
 };
